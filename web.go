fd9f1f3a512186cf2e18d5b03ac5e4ae83d92d6a
Rick Brunstedt <rbrunstedt@gmail.com>
2024-01-11 16:44:44 +0100

    wrapping handlerFunc

--- web.go
+++ web.go
@@ -3,6 +3,8 @@ package main
 import (
 	"app/web/router"
 	"app/web/router/middlewares"
+	"fmt"
+	"log"
 )
 
 var PORT = ":8080"
@@ -40,47 +42,35 @@ func main() {
 		data := `{"status": "ok"}`
 		ctx.Json(data)
 	})
+	rt.Get("/return-struct", func(ctx router.RouteCtx) {
+		data := Person{Name: "John", Age: 30}
+		ctx.Json(data)
+	})
+	rt.Get("/query", func(ctx router.RouteCtx) {
+		query := ctx.R.URL.Query()
+		ctx.Json(query)
+	})
+	rt.Get("/test-post", func(ctx router.RouteCtx) {
+		log.Println("GET /test-post")
+		html := `
+			<main>
+				<h1>Test POST</h1>
+				<form action="/test-post" method="post">
+					<input type="text" name="name" />
+					<input type="submit" value="Submit" />
+				</form>
+			</main>
+		`
+		ctx.Html(html)
+	})
+	rt.Post("/test-post", func(ctx router.RouteCtx) {
+		log.Println("POST /test-post")
+		name := ctx.R.FormValue("name")
+		ctx.W.Header().Set("Content-Type", "text/html; charset=utf-8")
+		fmt.Fprintf(ctx.W, "Hello %s", name)
+	})
 
-	// rt.Get("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprint(w, `
-	// 		<h1>Home</h1>
-	// 		<ul>
-	// 		<li><a href="/data">Data</a></li>
-	// 		<li><a href="/query?foo=bar&foo=test">Query test</a></li>
-	// 		<li><a href="/return-struct">Return struct as json</a></li>
-	// 		<li><a href="/test-post">Test POST</a></li>
-	// 		</ul>
-	// 	`)
-	// })
-	// rt.Get("/data", func(w http.ResponseWriter, r *http.Request) {
-	// 	data := `{"status": "ok"}`
-	// 	rt.Json(w, data)
-	// })
-	// rt.Get("/return-struct", func(w http.ResponseWriter, r *http.Request) {
-	// 	data := Person{Name: "John", Age: 30}
-	// 	rt.Json(w, data)
-	// })
-	// rt.Get("/query", func(w http.ResponseWriter, r *http.Request) {
-	// 	query := r.URL.Query()
-	// 	rt.Json(w, query)
-	// })
-	// rt.Get("/test-post", func(w http.ResponseWriter, r *http.Request) {
-	// 	log.Println("GET /test-post")
-	// 	fmt.Fprint(w, `
-	// 		<main>
-	// 			<h1>Test POST</h1>
-	// 			<form action="/test-post" method="post">
-	// 				<input type="text" name="name" />
-	// 				<input type="submit" value="Submit" />
-	// 			</form>
-	// 		</main>
-	// 	`)
-	// })
-	// rt.Post("/test-post", func(w http.ResponseWriter, r *http.Request) {
-	// 	log.Println("POST /test-post")
-	// 	name := r.FormValue("name")
-	// 	fmt.Fprintf(w, "Hello %s", name)
-	// })
+	log.Println("Listening on port", PORT)
 
 	err := rt.Start(PORT)
 	if err != nil {
